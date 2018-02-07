7ce7a9ae9dbd0a92d434ba714e1fd4732927edb2
Lj-PC\Lj <948115472@qq.com>
2018-02-07 23:12:43 +0800

    添加注册功能

--- src/main/java/jac/Login.java
+++ src/main/java/jac/Login.java
@@ -5,5 +5,6 @@ public class Login {
 	public Login() {
 		// TODO Auto-generated constructor stub
 		System.out.println("111111111111111");
+		
 	}
 }
---
+++ src/main/java/jac/Register.java
@@ -0,0 +1,10 @@
+package jac;
+
+public class Register {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("зЂВс111111111111");
+	}
+
+}
