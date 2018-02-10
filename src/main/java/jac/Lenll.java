837ed374ba3232fc18b9db6976345f7ea50d832e
Lj-PC\Lj <948115472@qq.com>
2018-02-10 21:14:00 +0800

    添加支付环节的代码

---
+++ src/main/java/jac/Lenll.java
@@ -0,0 +1,7 @@
+package jac;
+
+public class Lenll {
+	public static void main(String[] args) {
+		System.out.println("贷款");
+	}
+}
---
+++ src/main/java/jac/Pay.java
@@ -0,0 +1,7 @@
+package jac;
+
+public class Pay {
+	public static void main(String[] args) {
+		System.out.println("支付环节");
+	}
+}
